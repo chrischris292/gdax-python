1b80fd719fe224a72e836207200f481e21a93ec1
chrischris292 <cbchan2@illinois.edu>
2017-12-10 14:14:49 -0800

    Save actual json

--- gdax/collector.py
+++ gdax/collector.py
@@ -8,12 +8,12 @@ class Collector:
 
     def on_incremental_message(self, message):
         data = {"Type": "Incremental", "Message": message}
-        self.file.write(str(data).strip("\n"))
+        self.file.write(json.dumps(data).strip("\n"))
         self.file.write("\n")
 
     def on_snapshot_gap(self, message):
         data = {"Type": "Snapshot", "Message": message}
-        self.file.write(str(data).strip("\n"))
+        self.file.write(json.dumps(data).strip("\n"))
         self.file.write("\n")
 
     def on_close(self):
